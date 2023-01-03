e24ed246495b85082334f3a10ff7d260cd1d55b9
Anthony <anth06ny@gmail.com>
2023-01-03 15:28:38 +0100

    Correction TP Fonction

---
+++ app/src/main/java/com/amonteiro/a2023_01_epsi/constants.kt
@@ -0,0 +1,5 @@
+package com.amonteiro.a2023_01_epsi
+
+const val PRICE_SAND = 4
+const val PRICE_CROI = 0.95
+const val PRICE_BAG = 1.1
--- app/src/main/java/com/amonteiro/a2023_01_epsi/exoKotlin.kt
+++ app/src/main/java/com/amonteiro/a2023_01_epsi/exoKotlin.kt
@@ -1,5 +1,14 @@
 package com.amonteiro.a2023_01_epsi
 
 fun main() {
-    println("hello")
+    var res = boulangerie(nbSan = 4)
+    println(res)
+
 }
+
+fun boulangerie(nbCroi: Int = 0, nbBag: Int = 0, nbSan: Int=0) = nbCroi * PRICE_CROI + nbBag * PRICE_BAG + nbSan * PRICE_SAND
+
+
+fun min(a: Int, b: Int, c: Int): Int = if (a <= b && a <= c) a else if (b <= a && b <= c) b else c
+fun pair(c: Int) = c % 2 == 0
+fun myPrint(text: String) = println("##$text##")
